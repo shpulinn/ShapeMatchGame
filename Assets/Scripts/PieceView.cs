a23fec508b28371ab914ce1fe35ce1269405b98f
Nikita Shpulin <shpulinn@mail.ru>
2025-05-18 12:58:57 +0200

    Update PieceView.cs
    some tween added

--- Assets/Scripts/PieceView.cs
+++ Assets/Scripts/PieceView.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PieceView : MonoBehaviour
 {
@@ -25,10 +26,14 @@ public class PieceView : MonoBehaviour
         AnimalImage.sprite = visualDB.GetAnimal(data.Animal);
         Frame.sprite = visualDB.GetBorderShape(data.Shape);
         Frame.color = visualDB.GetColor(data.Color);
+
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        transform.DOScale(Vector3.one, .2f);
     }
     
     public void Clear()
     {
+        transform.DOScale(Vector3.zero, .2f);
         ShapeImage.sprite = null;
         Frame.sprite = null;
         gameObject.SetActive(false);
